f4f4e1ac55a075a4f5bf4487ad8517b1b49f543e
Anderson <andersonsq.andrade@gmail.com>
2020-10-31 22:00:08 +0100

    teste sem std::

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;    //com isso não preciso mais usar std::
+
 int main() {
 
-    std::cout << "Hello World! \n" << std::endl;
+    //std::cout << "Hello World! \n" << "Meu amigo" << std::endl;
+    cout << "Hello World! \n" << "Meu amigo." << endl;
 
     return 0;
 }
