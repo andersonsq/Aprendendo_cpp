20757d670fe1b72a142ed8ffe75ab3daab3c148c
Anderson <andersonsq.andrade@gmail.com>
2020-11-03 14:58:42 +0100

    criei um arquivo com variaveis inteiras

---
+++ variaveis.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+int idade;
+
+std::cout<<"insira a sua idade: "<<std::endl;
+std::cin >> idade;
+cout << "Sua idade é: "<< idade <<std::endl;
+
+return 0;
+}
