48c7d452c8cf0551e8a1897b5d5dca0f558273b2
Anderson <andersonsq.andrade@gmail.com>
2020-11-03 17:45:53 +0100

    Adicionei também a lógica while

--- condicoes.cpp
+++ condicoes.cpp
@@ -19,9 +19,20 @@ if (dinheiro < 10)
     {
     cout << "dinheiro insuficiente para ir ao cinema " << endl;
     }   
-else
+else if (dinheiro >10)
     {
     printf("dinheiro suficiente para ir ao cinema \n");
     }
+else if (dinheiro == 10)
+    {
+    std::cout << "dinheiro exatamente igual a "<<dinheiro <<std::endl;
+    }
+
+while ((dinheiro %2 == 0) &&  (dinheiro < 20))
+    {
+    cout << "Teu dinheiro é par e menor que 20" << endl;
+    break;
+    }
+
     return 0;
 }
